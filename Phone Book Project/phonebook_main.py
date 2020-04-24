5ef9a586aa6ca99fd9987778ac651f570d6e70ed
jaresj <60714611+jaresj@users.noreply.github.com>
2020-04-23 17:43:05 -0700

    Phonebook Project

---
+++ Phone Book Project/phonebook_main.py
@@ -0,0 +1,67 @@
+# Python Ver:   3.8.2
+#
+# Author:       Justice
+#
+# Purpose:      Phonebook Demo. Demonstrating OOP, Tkinter GUI module,
+#               using Tkinter Parent and Child relationships
+#
+# Tested OS:    This code was written and tested to work with windows 10.
+
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+
+
+# Be sure to import our other modules
+# so we can have access to them
+import phonebook_gui
+import phonebook_func
+
+
+# Frame is the Tkinter frame class that our own class will inherit from
+class ParentWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+
+        # define our master frame configuration
+        self.master = master
+        self.master.minsize(500,300) #(Height, Width)
+        self.master.maxsize(500,300)
+        # This CenterWindow method will center our app on the user's screen
+        phonebook_func.center_window(self,500,300)
+        self.master.title("The Tkinter Phonebook Demo")
+        self.master.configure(bg="#F0F0F0")
+        # This protocol method is a tkinter built-in method to catch if
+        # the user clicks the upper corner, "X" on Windows OS.
+        self.master.protocol("WM_DELETE_WINDOW", lambda: phonebook_func.ask_quit(self))
+        arg = self.master
+
+        # load in the GUI widgets from a separate module,
+        # keeping your code comparmentalized and clutter free
+        phonebook_gui.load_gui(self)
+
+     
+
+"""
+    It is from these few lines of code that python will begin our gui and application
+    The (if __name__ == "__main__":) part is basically telling Python that if this script
+    is ran, it should start by running the code below this line....in this case we have
+    instructed Python to run the following in this order
+
+    root = tk.Tk()               #This instantiates the Tk.() root frame (window into being
+    App = ParentWindow(root)     #This instantiates our own class as an App object
+    root.mainloop()              #This ensures the Tkinter class object, our window, to keep looping
+                                 #meaning, it will stay open until we instruct it to close
+"""
+ 
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
